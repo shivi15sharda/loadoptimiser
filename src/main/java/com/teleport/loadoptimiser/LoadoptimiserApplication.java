728612d6bb910046777376c2da783986f23e60a0
Snigdha Kashyap <shivi2001sn@gmail.com>
2026-01-16 23:19:11 +0530

    Initial commit

---
+++ src/main/java/com/teleport/loadoptimiser/LoadoptimiserApplication.java
@@ -0,0 +1,13 @@
+package com.teleport.loadoptimiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadoptimiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadoptimiserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teleport/loadoptimiser/LoadoptimiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teleport.loadoptimiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadoptimiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
